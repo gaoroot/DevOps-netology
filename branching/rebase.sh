c0d0a41a338372c8bd0623bd6c9f2185a58cc471
gaoroot <govorinao@gmail.com>
2021-11-01 18:14:07 +0800

    merge git-rebase

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,16 +3,8 @@
 
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 echo "====="
 
 done
-=======
-    echo "Parameter: $param"
-    count=$(( $count + 1 ))
-done
-
-echo "====="
->>>>>>> 1940d32 (git-rebase 1)
