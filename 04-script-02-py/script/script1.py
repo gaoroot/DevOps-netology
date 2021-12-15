ad10e4528924d7c4b75f73d1f5b15506505bfd9d
Alexandr Govorin <govorinao@gmail.com>
2021-12-15 22:40:49 +0800

    04-script-02-py

---
+++ 04-script-02-py/script/script1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import os
+
+bash_command = ["cd /run/media/alexandr/Data/DevOps/DevOps-netology", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+for result in result_os.split('\n'):
+    if result.find('изменено') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(prepare_result)
